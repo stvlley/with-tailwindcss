1dadafb6e4e6053eae91c0587ec809826d151f21
Stephen <stvlley.amzn@gmail.com>
2022-05-19 23:21:51 -0400

    stay dedicated

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,10 +4,10 @@ import Image from 'next/image'
 
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div >
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Stephen Talley</title>
+        <link rel="icon" href="/st2.svg" />
       </Head>
 
       <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
@@ -68,17 +68,12 @@ const Home: NextPage = () => {
         </div>
       </main>
 
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+      <div className="bg-[#0a192f] flex min-h-screen flex-col items-center justify-center py-2">
+       <footer className=" text-white flex  border-r items-center justify-center border-t">
           Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </a>
+          <Image src="/st2.svg" alt="Vercel Logo" width={72} height={50} />
       </footer>
+    </div>
     </div>
   )
 }
